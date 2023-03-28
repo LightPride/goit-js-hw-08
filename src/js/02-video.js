2cd2dfc133865574e7a16ecf25ab79df3e1f08ca
LightPride <vlad.grinevich1@gmail.com>
2023-03-28 22:00:55 +0300

    2 of 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+function onPlay(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds)
+}
+setCurrentTime();
+function setCurrentTime() {
+  
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+};
