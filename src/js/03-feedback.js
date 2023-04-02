304644418165f35efcf15d42b8e6f057fcaca192
LightPride <vlad.grinevich1@gmail.com>
2023-04-02 20:04:35 +0300

    3rd fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,14 +5,14 @@ feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 feedbackForm.addEventListener('submit', onFormSubmit);
 
 const STORAGE_KEY = 'feedback-form-state';
-
+let formElems = {};
 onFormDataCheck();
-let formElems = {}
+
 function onFormInput(event) {
   formElems = {
     email: feedbackForm.email.value,
     message: feedbackForm.message.value,
-  }
+  };
   const formDataJSON = JSON.stringify(formElems);
   localStorage.setItem(STORAGE_KEY, formDataJSON);
 }
@@ -20,14 +20,14 @@ function onFormInput(event) {
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
   console.log(formElems);
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormDataCheck() {
   const savedFormData = localStorage.getItem(STORAGE_KEY);
   if (savedFormData) {
-     const formElems = JSON.parse(savedFormData);
+    formElems = JSON.parse(savedFormData);
     for (let key in formElems) {
       feedbackForm[key].value = formElems[key];
     }
